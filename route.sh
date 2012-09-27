5b53d6703a362e6a889af8b63f9b40e4d21e7779
zijiao <admin@enjoydiy.comm>
2012-09-27 19:57:07 +0800

    routes for openvpn  2012-09-27

--- route.sh
+++ route.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 cd /root/route
-python chnroutes.py -p linux
+linux=python chnroutes.py -p linux
 zip -q -r linux.zip i*
-python chnroutes.py -p win
+win=python chnroutes.py -p win
 zip -q -r windows.zip v*
-python chnroutes.py -p openvpn
+openvpn=python chnroutes.py -p openvpn
 zip -q -r openvpn.zip rou*
 git add linux.zip ip*
-git commit -m "routers for linux `date +%Y-%m-%d`"
+git commit -m "routers for linux $linux `date +%Y-%m-%d`"
 git add windows.zip vp*
-git commit -m "routes for windows `date +%Y-%m-%d`"
+git commit -m "routes for windows $win  `date +%Y-%m-%d`"
 git add openvpn.zip route*
-git commit -m "routes for openvpn `date +%Y-%m-%d`"
+git commit -m "routes for openvpn $openvpn `date +%Y-%m-%d`"
 git push origin master
